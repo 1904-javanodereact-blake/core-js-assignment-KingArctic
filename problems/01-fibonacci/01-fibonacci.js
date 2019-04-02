ec90eb32ddbf96da4ee426212f816f369fc99430
clyons <austinlyns@gmail.com>
2019-04-02 16:06:48 -0400

    Added functionality to the Fibonacci Sequence function.

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,31 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
-
+function fib(n) 
+{
+    let fib1 = 0;
+    let fib2 = 0;
+    let fib3 = 0;
+    for (let x = 0; x < n; x++)
+    {
+        //console.log(fib2);
+        if (x == 0)
+        {
+            fib2 = 1;
+        }
+        else
+        {
+            fib3 = fib2 + fib1;
+            fib1 = fib2;
+            fib2 = fib3;
+        }
+    }
+    return fib3;
 }
+
+for (let y = 0; y < 10; y++)
+{
+    let num = Math.floor(Math.random() * 20);
+    console.log(`Term ${num} is ${fib(num)} in the Fibonacci sequence`);
+}
+
