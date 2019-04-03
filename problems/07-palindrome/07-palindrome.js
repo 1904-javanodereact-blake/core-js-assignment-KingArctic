c384b3dcb83e1a1a190b3a557b161a0cd8f060c2
clyons <austinlyns@gmail.com>
2019-04-03 12:58:57 -0400

    Palindrome now works with any input

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -3,34 +3,40 @@ Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) 
 {
-    if (typeof(someStr) !== typeof(''))
-    
-    let pali = false;
-    someStr = someStr.toString();
-    someStr = someStr.toLowerCase();
-    someStr = someStr.replace(/\s/g,'');
-    
-    console.log(someStr);
-    for (let x = 0; x < (someStr.length / 2); x++)
+    if (!isNaN(someStr))
     {
-        let temp1 = someStr[x];
-        let temp2 = someStr[someStr.length - (x + 1)];
-
-       // console.log(`${temp1} and ${temp2}`);
-        
-        if (temp1 === temp2)
+        if(someStr !== null) 
+            someStr = someStr.toString(); 
+    }
+    if (typeof(someStr) == typeof(''))
+    {
+        let pali = false;
+        someStr = someStr.toLowerCase();
+        someStr = someStr.replace(/\s/g,'');
+        for (let x = 0; x < (someStr.length / 2); x++)
         {
-            pali = true;
+            let temp1 = someStr[x];
+            let temp2 = someStr[someStr.length - (x + 1)];
+
+        // console.log(`${temp1} and ${temp2}`);
+            
+            if (temp1 === temp2)
+            {
+                pali = true;
+            }
+            else
+                return false;
         }
-        else
-            return false;
+
+        return pali;
     }
 
-    return pali;
+    return false
 }
 
 console.log(`Racecar palindrome test returns ${isPalindrome('RaCEcAr')}`);
-console.log(`Fox palindrome test returns ${isPalindrome('Fox')}`);
+console.log(`[1,2,1] palindrome test returns ${isPalindrome([1,2,1])}`);
 console.log(`undefined palindrome test returns ${isPalindrome(undefined)}`);
 console.log(`Hello olleh palindrome test returns ${isPalindrome('Hello olleh')}`);
 console.log(`5005 palindrome test returns ${isPalindrome(5005)}`);
+console.log(`Re,,,,,,D     RuM Sir            Is mUrdEr palindrome test returns ${isPalindrome('ReD     RuM Sir            Is mUrdEr')}`)
