e2123442e389af35f31271c424c339486c597aac
clyons <austinlyns@gmail.com>
2019-04-03 13:21:20 -0400

    Reverse String is fully functional.

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -3,5 +3,32 @@ Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr)
 {
-    
+    let newStr = '';
+    if (!isNaN(someStr))
+    {
+        if(someStr !== null) 
+            someStr = someStr.toString(); 
+    }
+    if (typeof(someStr) == typeof(''))
+    {
+        for (let x = (someStr.length - 1); x > -1; x--)
+        {
+            //console.log(someStr[x]);
+            newStr += someStr[x];
+            //console.log(newStr);
+        }
+
+        return newStr;
+    }
+
+    return '"Uh Oh! You inputed something invalid"';
 }
+
+console.log(`Hello reversed is ${reverseStr('Hello')}`);
+console.log(`World reversed is ${reverseStr('World')}`);
+console.log(`123456789 reversed is ${reverseStr(123456789)}`);
+console.log(`[2,3,2,3] reversed is ${reverseStr([2,3,2,3])}`);
+console.log(`null reversed is ${reverseStr(null)}`);
+console.log(`NaN reversed is ${reverseStr(NaN)}`);
+console.log(`undefined reversed is ${reverseStr(undefined)}`);
+console.log(`SpAcEs a  n  d       Caps reversed is ${reverseStr('SpAcEs a  n  d       Caps')}`);
